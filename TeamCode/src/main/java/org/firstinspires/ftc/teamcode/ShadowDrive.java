87fca6a84317c0141b35206446c42c91f430136e
2027202 <2027202@creightonprep.org>
2025-09-06 12:42:39 -0500

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShadowDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShadowDrive.java
@@ -29,7 +29,6 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -55,7 +54,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="ShadowDrive", group="Linear OpMode")
-public class BasicOmniOpMode_Linear extends LinearOpMode {
+public class ShadowDrive extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
